67c7ea0298b22463c35c48e80e696d531ca30236
Dplo1514 <dladlsgur3334@gmail.com>
2022-10-18 23:55:26 +0900

    BUGFIX/임인혁/AllArgs & NoArgs 멘토링

--- src/main/java/com/example/team6/domain/Comment.java
+++ src/main/java/com/example/team6/domain/Comment.java
@@ -1,7 +1,6 @@
 package com.example.team6.domain;
 
 import com.example.team6.controller.request.CommentRequestDto;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import java.time.LocalDateTime;
 
 //@Builder
 //@Getter
@@ -35,10 +33,7 @@ public class Comment extends Timestamped {
   @Column(nullable = false)
   private String content;
 
-  public Comment() {
-
-  }
-
+  //getter
   public Long getId() {
     return id;
   }
@@ -55,18 +50,45 @@ public class Comment extends Timestamped {
     return content;
   }
 
+  // setter
+  public void setMember(Member member) {
+    this.member = member;
+  }
+
+  public void setPost(Post post) {
+    this.post = post;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  // 모든 파라미터가 필요한 생성자
   public Comment(Member member, Post post, String content) {
-    this.id = id;
+//    this.id = id; FIXME : 확인 요함
     this.member = member;
     this.post = post;
     this.content = content;
   }
 
-  public void update(CommentRequestDto commentRequestDto) {
+  // 모든 파라미터가 필요 없는 생성자
+  public Comment() {
+  }
+
+  // Comment 객체에 필드 값 할당
+  public void saveComment(CommentRequestDto commentRequestDto , Member member , Post post) {
+    this.member = member;
+    this.post = post;
     this.content = commentRequestDto.getContent();
   }
 
+  public void updateComment(CommentRequestDto commentRequestDto) {
+    this.content = commentRequestDto.getContent();
+  }
+
+  // 댓글을 수정한다거나 삭제할 때에 댓글의 작성자와 로그인한 멤버가 일치하는지 비교 boolean return
   public boolean validateMember(Member member) {
-    return !this.member.equals(member);
+    // 일치하면 true를 리턴 : 일치하지 않으면 false를 리턴
+    return this.member.equals(member) ? true : false ;
   }
 }
